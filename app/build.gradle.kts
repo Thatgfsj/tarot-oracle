8da51e6f979d200ebbb55aefa188079dc7e32b53
Thatgfsj <Thatgfsj@users.noreply.github.com>
2026-07-03 06:14:40 +0800

    chore: version 0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.thatgfsj.tarot"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.1.2"
+        versionCode = 3
+        versionName = "0.1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
